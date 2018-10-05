c507035e77783012931d0990381986839d962766
vldmr1 <vldmr.one@gmail.com>
2018-10-05 14:04:47 +0300

    advanced-zeros solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const factors = (base) => {
+    let factors = [];
+    for (let i = 2; i <= base; i++) {
+        while ((base % i) === 0) {
+            factors.push(i);
+            base /= i;
+        }
+    }
+    return factors;
+  }
+
+  let counts = {};
+    let num;
+    for (let i = 0; i < factors(base).length; i++) {
+      num = factors(base)[i];
+      counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+  let sumArray = [],
+      elementsSum = 0,
+      n = 1;
+  for (let e in counts) {
+    while (Math.floor(number / Number(e)**n >= 1)) {
+      elementsSum += Math.floor(number / Number(e)**n);
+      n++;
+    }
+    elementsSum /= counts[e];
+    sumArray.push(Math.floor(elementsSum));
+    n = 1;
+    elementsSum = 0;    
+  }  
+return Math.min.apply(null, sumArray);
 }
